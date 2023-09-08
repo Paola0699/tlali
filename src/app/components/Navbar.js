ba2d9e7589134c667696a24381afb6fcdf84b318
Paola Perez Valencia <paola.pv1999@gmail.com>
2023-09-08 16:06:07 -0600

    UPDATE Settings removed

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -140,7 +140,7 @@ const Navbar = () => {
             ))}
           </Box>
 
-          <Box sx={{ flexGrow: 0 }}>
+         {/*  <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
@@ -168,7 +168,7 @@ const Navbar = () => {
                 </MenuItem>
               ))}
             </Menu>
-          </Box>
+          </Box> */}
         </Toolbar>
       </Container>
     </AppBar>
