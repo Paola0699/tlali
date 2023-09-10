c33b8fdf2c36b9e5a2b3538ed8b95f617d0df7f8
Paola Perez Valencia <paola.pv1999@gmail.com>
2023-09-10 10:16:43 -0600

    UPDATE Category changed

--- public/json/mexico.js
+++ public/json/mexico.js
@@ -3,78 +3,78 @@
         "Platillo": "Sopa Azteca",
         "Descripcion": "Tradicional sopa de tortilla con nuestra mezcla de chiles, acompañada de aguacate y un toque de crema ácida",
         "Precio": 89,
-        "Categoria": "México a tu Paladar"
+        "Categoria": "Mexico a tu Paladar"
     },
     {
         "Platillo": "Chilaquiles tradicionales",
         "Descripcion": "Totopos bañados en la salsa de tu elección (verde o roja) acompañados de cebolla, cilantro y queso fresco",
         "Precio": 126,
-        "Categoria": "México a tu Paladar"
+        "Categoria": "Mexico a tu Paladar"
     },
     {
         "Platillo": "Chilaquiles Suizos",
         "Descripcion": "Bañados en nuestra exquisita salsa suiza, acompañados de pollo, gratinados con queso manchega",
         "Precio": 137,
-        "Categoria": "México a tu Paladar"
+        "Categoria": "Mexico a tu Paladar"
     },
     {
         "Platillo": "Enchiladas",
         "Descripcion": "Tortilla de maíz ligeramente frita, rellena de pollo deshebrado, bañadas en salsa verde o roja, lechuga, queso fresco rallado (4 pzas)",
         "Precio": 124,
-        "Categoria": "México a tu Paladar"
+        "Categoria": "Mexico a tu Paladar"
     },
     {
         "Platillo": "Enchiladas Suizas",
         "Descripcion": "Tortilla de maíz ligeramente frita, rellena de pollo, bañadas en salsa suiza de la casa, gratinadas con queso manchego (4 pzas)",
         "Precio": 131,
-        "Categoria": "México a tu Paladar"
+        "Categoria": "Mexico a tu Paladar"
     },
     {
         "Platillo": "Enchiladas Veggie",
         "Descripcion": "Tortilla de maíz rellana de queso panela bañadas en salsa verde o roja. acompañadas de lechuga, queso fresco, toque de crema y rodajas de cebolla (4 pzas)",
         "Precio": 131,
-        "Categoria": "México a tu Paladar"
+        "Categoria": "Mexico a tu Paladar"
     },
     {
         "Platillo": "Tlacoyos ahogados",
         "Descripcion": "En salsa verde de la casa, lechuga, queso fresco y un toque de crema ácida",
         "Precio": 106,
-        "Categoria": "México a tu Paladar"
+        "Categoria": "Mexico a tu Paladar"
     },
     {
         "Platillo": "Enfrijoladas",
         "Descripcion": "Tortilla de maíz, rellena de pollo bañada en salsa de frijol, acompañadas de queso fresco, toque de crema y chorizo (4pzas)",
         "Precio": 128,
-        "Categoria": "México a tu Paladar"
+        "Categoria": "Mexico a tu Paladar"
     },
     {
         "Platillo": "Huarache",
         "Descripcion": "Cubierto de frijoles de la casa, con arrachera, acompañado de tomates cherry, cebollita cambray, queso fresco y guacamole",
         "Precio": 119,
-        "Categoria": "México a tu Paladar"
+        "Categoria": "Mexico a tu Paladar"
     },
     {
         "Platillo": "Alambre",
         "Descripcion": "Exquisita carne enchilada, con cebolla, pimiento, tocino y gratinado con queso asadero, acompañado de tortillas de harina",
         "Precio": 137,
-        "Categoria": "México a tu Paladar"
+        "Categoria": "Mexico a tu Paladar"
     },
     {
         "Platillo": "Molletes",
         "Descripcion": "Mini bolillo con frijoles refritos de la casa, gratinados con queso, manchego y pico de gallo. $23 más agrega tocino, jamón, chorizo o champiñones. $39 más agrega arrachera. *No aplica la promoción Martes y miércoles\"",
         "Precio": 87,
-        "Categoria": "México a tu Paladar"
+        "Categoria": "Mexico a tu Paladar"
     },
     {
         "Platillo": "Tlacoyos",
         "Descripcion": "Tradicionales de masa azul acompañados de lechuga, queso fresco y un toque de crema ácida",
         "Precio": 88,
-        "Categoria": "México a tu Paladar"
+        "Categoria": "Mexico a tu Paladar"
     },
     {
         "Platillo": "Sopes",
         "Descripcion": "Clásicos sopes con salsa de tu elección (verde o roja), acompañados de queso fresco, un toque de crema ácida, cebollita y cilantro. Puedes agregar la proteína de tu elección (pollo, cecina, arrachera, carne enchilada, chorizo) por $41 más",
         "Precio": 85,
-        "Categoria": "México a tu Paladar"
+        "Categoria": "Mexico a tu Paladar"
     }
 ]
